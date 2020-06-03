d4ebfb4d61afb40a058119a41bdc96506a0603b4
Erik Peterson <Erik.Exists@gmail.com>
2020-06-03 10:38:13 -0700

    - Rename run_exp to run_co - Add exp0 recipe - Add exp0 analysis notebook - Update vis in the oc testing notebook

--- theoc/run_oc.py
+++ theoc/run_oc.py
@@ -19,12 +19,13 @@ def main(name, num_trials=20, verbose=False, **oc_kwargs):
 
     if verbose: print(f">>> Running: {name}")
 
-    # Output structures
+    # Init
     d_H = defaultdict(list)
     d_MI = defaultdict(list)
+    d_dMI = defaultdict(list)
     d_PAC = defaultdict(list)
-    d_rate = defaultdict(list)
-    d_dist = defaultdict(list)
+    d_py = defaultdict(list)
+    d_pxy = defaultdict(list)
 
     # -- Run
     iterations = range(num_trials)
@@ -32,31 +33,47 @@ def main(name, num_trials=20, verbose=False, **oc_kwargs):
         oc_kwargs["stim_seed"] = i
         res = oscillatory_coupling(**oc_kwargs)
 
-        # Save parts of the result
+        # Save parts of the result...
+        #
+        # Entopy
         for b in res['H'].keys():
             d_H[b].append(res["H"][b])
+        d_H["trial"].append(i)
+
+        # MI
         for b in res['MI'].keys():
             d_MI[b].append(res['MI'][b])
+        d_MI["trial"].append(i)
+
+        # Change in MI, dMI
+        for b in res['dMI'].keys():
+            d_dMI[b].append(res['dMI'][b])
+        d_dMI["trial"].append(i)
+
+        # p(y) (oc'ed)
+        for b in res['p_y'].keys():
+            d_py[b].extend(res['p_y'][b])
+        d_py["m"].extend(range(1, oc_kwargs["m"] + 1))
+        d_py["trial"].extend([i] * oc_kwargs["m"])
+
+        # PAC
         for b in res['PAC'].keys():
             d_PAC[b].append(res['PAC'][b])
-        for b in res['dist'].keys():
-            d_dist[b].extend(res['dist'][b])
-        d_dist["m"].extend(range(1, oc_kwargs["m"] + 1))
-        for b in res['spikes'].keys():
-            mrate = np.mean(res['spikes'][b].sum(0) / float(oc_kwargs["t"]))
-            d_rate[b].append(mrate)
+        d_PAC["trial"].append(i)
 
-    # -- To disk!
+    # -- Dump to disk!
     df_H = pd.DataFrame(d_H)
     df_H.to_csv(name + "_H.csv", index=False)
     df_MI = pd.DataFrame(d_MI)
     df_MI.to_csv(name + "_MI.csv", index=False)
+    df_dMI = pd.DataFrame(d_dMI)
+    df_dMI.to_csv(name + "_dMI.csv", index=False)
+    df_py = pd.DataFrame(d_py)
+    df_py.to_csv(name + "_py.csv", index=False)
+    df_pxy = pd.DataFrame(d_pxy)
+    df_pxy.to_csv(name + "_pxy.csv", index=False)
     df_PAC = pd.DataFrame(d_PAC)
     df_PAC.to_csv(name + "_PAC.csv", index=False)
-    df_rate = pd.DataFrame(d_rate)
-    df_rate.to_csv(name + "_rate.csv", index=False)
-    df_dist = pd.DataFrame(d_dist)
-    df_dist.to_csv(name + "_dist.csv", index=False)
 
     return None
 
